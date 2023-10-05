40c7bb475f8a080a0625e3e58f41b3350e386996
Gabriel <gluis.sousa.0@gmail.com>
2023-10-05 01:54:20 -0300

    Estruturas de repeticoes

---
+++ aula6/Controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,17 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        if(plano == "B"){
+            System.out.println("100 minutos de ligacao");
+        }else if (plano == "M"){
+            System.out.println("100 minutos de ligacao");
+            System.out.println("whats e instagram gratis");
+        }else if(plano == "T"){
+            System.out.println("100 minutos de ligacao");
+            System.out.println("whats e instagram gratis");
+            System.out.println("5Gb youtube");
+        
+        }
+    }
+}
